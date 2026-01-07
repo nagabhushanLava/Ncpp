a768cf6184815b103f5ee7808f77215f474f65e8
nagabhshan <nagabhushan421@gmail.com>
2026-01-07 12:06:23 +0530

    adding

--- cpp/Lambda function/l1.cpp
+++ cpp/Lambda function/l1.cpp
@@ -44,9 +44,37 @@ int main()
     };
     
     cout<<s4(a1,a2);
-    cout<<s4(45.8,78);
+    cout<<s4(45.8,78)<<endl;
     
-
-
+    
+    vector<int> v;
+    v={10,1,2,4,3,7,6,8,9,5};
+    
+    sort(v.begin(),v.end(),[](int a,int b){
+        return a<b;
+    });
+    for(auto i:v)
+    {
+        cout<<i<<" ";
+    }
+    cout<<endl;
+    
+    
+    for_each(v.begin(),v.end(),[](int x){
+        cout<<x<<" ";
+        
+    });
+    cout<<endl;
+    
+    auto y=find_if(v.begin(),v.end(),[](int x){
+        return x==5;
+        
+    });
+    cout<<"i "<<*y<<endl;
+    
+    auto u=count_if(v.begin(),v.end(),[](int g){
+        return g>1;
+    });
+    cout<<"u="<<u<<endl;
 return 0;
 }
