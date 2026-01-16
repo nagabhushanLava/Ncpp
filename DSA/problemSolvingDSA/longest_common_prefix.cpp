3b27c7f5a3044283e624b34cd0ef0c7b3fac823e
nagabhushanLava <56127932+nagabhushanLava@users.noreply.github.com>
2026-01-16 14:10:56 +0530

    adding problem solved

---
+++ DSA/problemSolvingDSA/longest_common_prefix.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+       
+    if (strs.empty())
+        return "";
+
+    string prefix = strs[0];
+
+    for (int i = 0; i < prefix.size(); i++)
+    {
+        char c = prefix[i];
+
+        for (int j = 1; j < strs.size(); j++)
+        {
+            if (i >= strs[j].size() || strs[j][i] != c)
+            {
+                return prefix.substr(0, i);
+            }
+        }
+    }
--- cpp/templeate/templet_pointerc1.cpp
+++ cpp/templeate/templet_pointerc1.cpp
@@ -59,5 +59,33 @@ cout<<"----------------------------------------------------"<<endl;
     cout<<b.readvalue()<<endl;
     cout<<f.readvalue()<<endl;
 
+
+
+
+        gen<int> *p=new gen<int>(500);
+        cout<<p->readvalue()<<endl;
+        delete p;
+        gen<double> *d=new gen<double>(9.8765);
+        cout<<d->readvalue()<<endl;
+        delete d;
+        gen<string> *s=new gen<string>("Taj Mahal");
+        cout<<s->readvalue()<<endl;
+        delete s;   
+        gen<char> *c=new gen<char>('A');
+        cout<<c->readvalue()<<endl; 
+        delete c;
+        gen<float> *fl=new gen<float>(3.1416f);
+        cout<<fl->readvalue()<<endl;
+        delete fl;
+        gen<long> *ln=new gen<long>(1234567890L);
+        cout<<ln->readvalue()<<endl;
+        delete ln;
+        gen<bool> *bl=new gen<bool>(true);
+        cout<<bl->readvalue()<<endl;
+        delete bl;
+        gen<short> *sh=new gen<short>(32000);
+        cout<<sh->readvalue()<<endl;        
+        delete sh;  
+
     return 0;
 }
